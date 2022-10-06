e99e4f103f46b7face73d635f82337873222c132
Md. Ibn Jihan <101347202+mdibnjihan@users.noreply.github.com>
2022-10-06 11:45:00 +0600

    Initial commit

---
+++ Password Generator/MainWindow.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Password_Generator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        int currentPasswordLength;
+        bool symbolRequired;
+        Random str = new Random();
+
+        private void passwordGenerator(int passwordLength)
+        {
+            String charcterString = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890|!#$%&/()=?»«@£§€{}.-;'<>_,";
+            String randomPassword = "";
+
+            for (int i = 0; i < passwordLength; i++)
+            {
+                int randomNum = str.Next(0, charcterString.Length);
+                randomPassword += charcterString[randomNum];
+            }
+
+            if (symbolRequired == true)
+            {
+                StringBuilder mbuilder = new StringBuilder(randomPassword);
+                mbuilder[mbuilder.Length - 1] = '@';
+                Generated_Password.Content = mbuilder.ToString();
+            }
+            else
+            {
+                Generated_Password.Content = randomPassword;
+            }
+        }
+        public MainWindow()
+        {
+            InitializeComponent();
+            Password_Length_Slider.Minimum = 8;
+            Password_Length_Slider.Maximum = 64;
+            passwordGenerator(16);
+        }
+
+        private void Copy_Button_Click(object sender, RoutedEventArgs e)
+        {
+            Clipboard.SetText((string)Generated_Password.Content);
+        }
+
+        private void Password_Length_Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            //Slider_Value_Text.Content = Password_Length_Slider.ValueChanged;
+            currentPasswordLength = (int)Password_Length_Slider.Value;
+            passwordGenerator(currentPasswordLength);
+        }
+
+        private void Regenerate_Password_Click(object sender, RoutedEventArgs e)
+        {
+            currentPasswordLength = (int)Password_Length_Slider.Value;
+            passwordGenerator(currentPasswordLength);
+        }
+
+        private void Symbol_Required_Checkbox_Checked(object sender, RoutedEventArgs e)
+        {
+            if (Symbol_Required_Checkbox.IsChecked == true)
+            {
+                symbolRequired = true;
+            }
+            else
+            {
+                symbolRequired = false;
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,15 @@
+# Password-Generator
+I tried to create a simple password generator application for Windows operating system.
+This is a fun project. It should not be used as a commercial or other type of uses.
+The code is open to public as anyone can copy or modify the codes.
+This Windows application is made using C#, XAML, .NET Framework.
+This application can run on Windows XP/ Vista/ 7/ 8.1/ 10/ 11.
+For runnning the application, .NET Framework 3.1 (LTS) or higher must be installed on the system.
+
+# Used Images
+password.png => <a href="https://www.flaticon.com/free-icons/password" title="password icons">Password icons created by Those Icons - Flaticon</a><br>
+github.png => <a href="https://www.flaticon.com/free-icons/github" title="github icons">Github icons created by Pixel perfect - Flaticon</a><br>
+Social Preview => <a href='https://www.freepik.com/vectors/cloud-storage'>Cloud storage vector created by vectorpouch - www.freepik.com</a><br>
+
+Built with 🤍 by Md. Ibn Jihan
+Take love and happy coding.
